6a2c4836fd0f91cb52468cc31c07e4222d1293ce
Michelle Binder <mbinder1@smail.uni-koeln.de>
2022-11-07 17:56:43 +0100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        darkblue: '#32475b',
+        mediumblue: '#3e566c',
+        red: '#ae131e',
+      },
+    },
+  },
+  plugins: [],
+}
