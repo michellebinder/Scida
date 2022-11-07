b6f373aabd710f35faf211550e7768ad2fbcc70b
Michelle Binder <mbinder1@smail.uni-koeln.de>
2022-11-07 17:53:05 +0100

    Add configuration

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
-import Head from 'next/head'
+import Head from "next/head";
+import React from "react";
+import Footer from "../components/footer";
+import Header from "../components/header";
+import Login from "../components/login";
 
-export default function Home() {
+/*
+function to create structure for our website
+*/
+function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Scida</title>
         <meta charset="utf-8" />
@@ -11,6 +18,18 @@ export default function Home() {
           rel="stylesheet"
         />
       </Head>
+      <div className="bg-[#bada55] flex flex-col h-screen justify-between">
+        <div id="start" className="fixed z-10">
+          <Header></Header>
+        </div>
+        <div className="font-sans font-family:Arial md:pl-20 md:pr-20 md:pt-36 pt-56 grid grid-cols-1 gap-10 p-5 z-0">
+          <Login></Login>
+          <Footer></Footer>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
+
+export default Home;
