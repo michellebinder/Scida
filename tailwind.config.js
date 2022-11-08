beda7e04d4ee6ed9776216617b9ef937a35bb5fa
Marc Peter <mpeter6@smail.uni-koeln.de>
2022-11-08 11:00:19 +0100

    added tailwind functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        darkblue: '#32475b',
-        mediumblue: '#3e566c',
-        red: '#ae131e',
+        transparent: "transparent",
+        current: "currentColor",
+        darkblue: "#32475b",
+        mediumblue: "#3e566c",
+        red: "#ae131e",
       },
     },
   },
   plugins: [],
-}
+};
