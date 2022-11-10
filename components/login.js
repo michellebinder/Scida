7a8db5caf9204538f842bfe9d730eeb754d644a4
feli.pn <fpreussn@smail.uni-koeln.de>
2022-11-10 22:18:44 +0100

    Add return button to login. Align.

--- components/login.js
+++ components/login.js
@@ -31,7 +31,7 @@ export default function Login() {
                         type="text"
                         class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                         id="exampleFormControlInput2"
-                        placeholder="Email Adresse"
+                        placeholder="Benutzername"
                       />
                     </div>
 
@@ -63,14 +63,37 @@ export default function Login() {
                       </a>
                     </div>
 
-                    <div class="text-center lg:text-left">
-                      <button
-                        type="button"
-                        class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                      >
-                        Login
-                      </button>
+                    
+
+                    {/* create two buttons next to each other and call them "back" and "forth"*/}
+                    <div class="flex justify-between items-center mb-6">
+                      <div class="form-group
+                      form-check">
+                        <button
+                          type="button"
+                          class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                          // when button is clicked return to localhost:3000
+                          onClick={() => (window.location.href = "/")}
+                        >
+                          Zurück
+                        </button>
+                      </div>
+                      <div class="form-group
+                      form-check">
+                        <button
+                          type="button"
+                          class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                          // when button is clicked return to localhost:3000
+                          onClick={() => (window.location.href = "/")}
+                        >
+                          Login
+                        </button>
+                      </div>
                     </div>
+
+
+
+
                   </form>
                 </div>
               </div>
--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,16 @@ import { motion } from 'framer-motion';
 
 function App({ Component, pageProps, router }) {
   return (
-    // <motion.div key={router.route}  initial="pageInitial" animate="pageAnimate" variants={{
-    //   pageInitial: {
-    //     opacity: 0
-    //   },
-    //   pageAnimate: {
-    //     opacity: 1
-    //   },
-    // }}>
+    <motion.div key={router.route}  initial="pageInitial" animate="pageAnimate" variants={{
+      pageInitial: {
+        opacity: 0
+      },
+      pageAnimate: {
+        opacity: 1
+      },
+    }}>
       <Component {...pageProps} />
-    // {/* </motion.div> */}
+    // </motion.div>
   )
 }
 
