eea732f85075313fd9e1b8dcc6a80c3c9b0f70f6
Michelle Binder <mbinder1@smail.uni-koeln.de>
2022-11-11 12:06:33 +0100

    Add blue part to header

--- components/header.js
+++ components/header.js
@@ -6,19 +6,22 @@ export default function Header() {
   return (
     <div>
       {/* red med fak bar */}
-      <div className="bg-red text-transparent font-bold font-apple text-xs p-0.02 w-screen flex md:flex-row flex-col items-center text-center">
+      <div className="bg-darkblue text-transparent font-bold font-apple text-xs p-1 w-screen flex md:flex-row flex-col items-center text-center">
         Dies ist ein Easter Egg
-      </div>
-      <div className="bg-slate-100 text-white font-bold font-apple text-xl pt-5 w-screen flex md:flex-row flex-col items-center text-center">
         <Link href="/">
           <div className="px-8 flex flex-row justify-center">
             <img
-              className="w-16 rounded-2xl"
+              className="w-8 rounded-2xl"
               src="SiegelMedFak.png"
               alt="Siegel der medizinischen Fakultät"
             />
           </div>
         </Link>
+      </div>
+      <div className="bg-red text-transparent font-bold font-apple text-xs p-0.02 w-screen flex md:flex-row flex-col items-center text-center">
+        Dies ist ein Easter Egg 2
+      </div>
+      <div className="bg-slate-100 text-white font-bold font-apple text-xl pt-5 w-screen flex md:flex-row flex-col items-center text-center">
         <Link href="/">
         {/* <div className="hover:underline">Scida @ Universität zu Köln</div> */}
         </Link>
