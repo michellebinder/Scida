e0e2b2df4ba9c7b1b4eab1abb720d65ec93fb6c5
Michelle Binder <mbinder1@smail.uni-koeln.de>
2022-11-16 15:25:41 +0100

    Update texts for lecturers and admins

--- pages/dashboardAdmin.js
+++ pages/dashboardAdmin.js
@@ -34,46 +34,46 @@ export default function Home() {
                 {/* single daisyUI card component  */}
                 <div className="card card-normal bg-primary text-primary-content">
                   <div className="card-body">
-                    <h2 className="card-title">Anwesenheit</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h2 className="card-title text-white">Anwesenheit</h2>
+                    <p>Scanne hier einen QR-Code, um deine Anwesenheit einzutragen!</p>
                     <div className="card-actions justify-end">
-                      {/* daisy UI radial progress component */}
-                      <div
+                      {/* <div
                         className="radial-progress"
                         style={{ "--value": 70 }}
                       >
                         70%
-                      </div>
+                      </div> */}
+                      <button className="btn">Öffnen</button>
                     </div>
                   </div>
                 </div>
                 {/* single daisyUI card component  */}
                 <div className="card card-normal bg-primary text-primary-content">
                   <div className="card-body">
-                    <h2 className="card-title">Meine Kurse</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h2 className="card-title text-white">Meine Kurse</h2>
+                    <p>Hier findest du alle Blockpraktika, für die du dich angemeldet hast.</p>
                     <div className="card-actions justify-end">
-                      <button className="btn">Buy Now</button>
+                      <button className="btn">Öffnen</button>
                     </div>
                   </div>
                 </div>
                 {/* single daisyUI card component  */}
                 <div className="card card-normal bg-primary text-primary-content">
                   <div className="card-body">
-                    <h2 className="card-title">Ausdrucke</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h2 className="card-title text-white">Ausdrucke</h2>
+                    <p>Hol dir die Bestätigung über deine Anwesenheit!</p>
                     <div className="card-actions justify-end">
-                      <button className="btn">Buy Now</button>
+                      <button className="btn">Öffnen</button>
                     </div>
                   </div>
                 </div>
                 {/* single daisyUI card component  */}
                 <div className="card card-normal bg-primary text-primary-content">
                   <div className="card-body">
-                    <h2 className="card-title">Hilfe</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h2 className="card-title text-white">Hilfe</h2>
+                    <p>Passwort vergessen oder Verbesserungsvorschlag? Hier entlang!</p>
                     <div className="card-actions justify-end">
-                      <button className="btn">Buy Now</button>
+                      <button className="btn">Öffnen</button>
                     </div>
                   </div>
                 </div>
