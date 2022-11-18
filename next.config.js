84d4b6c984f73dcc45cf1172cf9f3b2b8b470884
Marc Peter <mpeter6@smail.uni-koeln.de>
2022-11-18 12:51:18 +0100

    adds content to next config solve fs

--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ module.exports = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
 };
