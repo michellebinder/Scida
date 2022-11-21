5f53babb6590834b56fbc7bab0f6390d489a7fb4
Michelle Binder <mbinder1@smail.uni-koeln.de>
2022-11-21 13:45:33 +0100

    Add support page

---
+++ pages/support.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import React from "react";
+import NavbarAdmin from "../components/navbar";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Scida</title>
+        <meta charSet="utf-8" />
+      </Head>
+      {/* div that stretches from the very top to the very bottom */}
+      <div className="flex flex-col h-screen justify-between bg-base-100">
+        {/* dashboard navbar with navigation items  */}
+        <Navbar></Navbar>
+      </div>
+    </div>
+  );
+}
