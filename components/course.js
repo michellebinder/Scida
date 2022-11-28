a88f4d528d0bfe1935e2c9912f3165225e30cf16
feli.pn <fpreussn@smail.uni-koeln.de>
2022-11-29 00:20:56 +0100

    Different alignment.

--- components/course.js
+++ components/course.js
@@ -13,11 +13,13 @@ export default function Course({
         <div className="card card-normal bg-primary text-primary-content">
             <div className="card-body">
                 <div className="flex justify-between">
-                    <h2 className="card-title text-white">{courses}</h2>
-                    <div className="text-left ml-5">
-                        <h3 className="card-subtitle">Praktikums-ID: {praktID}</h3>
-                        <h3 className="card-subtitle">Woche: {week} </h3>
-                        <h3 className="card-subtitle">Gruppe: {group}</h3>
+                    <div className="card-actions flex flex-col justify-center">
+                        <h2 className="card-title text-white">{courses}</h2>
+                        <div className="text-left ml-5">
+                            <h3 className="card-subtitle">Praktikums-ID: {praktID}</h3>
+                            <h3 className="card-subtitle">Woche: {week} </h3>
+                            <h3 className="card-subtitle">Gruppe: {group}</h3>
+                        </div>
                     </div>
                     <div className="card-actions flex flex-col justify-center gap-5">
                         <div className="radial-progress"
@@ -27,7 +29,7 @@ export default function Course({
                             */}
                         </div>
                         <Link href="/courseList">
-                            <button className="btn border-transparent hover:border-transparent bg-neutral hover:bg-secondary text-background">Details</button>
+                            <button className="btn ml-5 border-transparent hover:border-transparent bg-neutral hover:bg-secondary text-background">Details</button>
                         </Link>
                     </div>
                 </div>
