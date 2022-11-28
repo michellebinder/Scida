652d7bf796abdd7854bbb3ecd54a463f1ddb8586
feli.pn <fpreussn@smail.uni-koeln.de>
2022-11-28 14:33:38 +0100

    Change button design.

--- components/course.js
+++ components/course.js
@@ -21,17 +21,25 @@ export default function Course({
                     viewBox="0 0 21 21"
                     stroke="white"
                     >
-                    </svg>
-                    
+                    </svg>                    
                     <div className="text-left">
                         <h3 className="card-subtitle">Praktikums-ID: {praktID}</h3>
                         <h3 className="card-subtitle">Woche: {week} </h3>
                         <h3 className="card-subtitle">Gruppe: {group}</h3>
                         <h3 className="card-subtitle">Anwesenheit: {attendance}</h3>
                     </div>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="32"
+                        height="32"
+                        viewBox="0 0 24 24"
+                        fill="white"
+                      >
+                        <path d="M19 2c1.654 0 3 1.346 3 3v14c0 1.654-1.346 3-3 3h-14c-1.654 0-3-1.346-3-3v-14c0-1.654 1.346-3 3-3h14zm0-2h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-8 8h-1v-2h1v1h2v1h-1v1h-1v-1zm2 12v-1h-1v1h1zm-1-15v-1h-2v1h1v1h1v-1zm8-1v6h-1v-1h-4v-5h5zm-1 4v-3h-3v3h3zm-14 2h-1v1h2v-1h-1zm0 3h1v1h1v-3h-1v1h-2v2h1v-1zm5 1v2h1v-2h-1zm4-10h-1v3h1v-3zm0 5v-1h-1v1h1zm3-2h1v-1h-1v1zm-10-1h-1v1h1v-1zm2-2v5h-5v-5h5zm-1 1h-3v3h3v-3zm9 5v1h-1v-1h-2v1h-1v-1h-3v-1h-1v1h-1v1h1v2h1v-1h1v2h1v-2h3v1h-2v1h2v1h1v-3h1v1h1v2h1v-1h1v-1h-1v-1h-1v-1h1v-1h-2zm-11 8h1v-1h-1v1zm-2-3h5v5h-5v-5zm1 4h3v-3h-3v3zm12-3v-1h-1v1h1zm0 1h-1v1h-1v-1h-1v-1h1v-1h-2v-1h-1v2h-1v1h-1v3h1v-1h1v-1h2v2h1v-1h1v1h2v-1h1v-1h-2v-1zm-9-3h1v-1h-1v1zm10 2v1h1v1h1v-3h-1v1h-1zm2 4v-1h-1v1h1zm0-8v-1h-1v1h1z" />
+                      </svg>
                     <div className="card-actions justify-end">
                     <Link href="/courseList">
-                        <button className="btn border-transparent bg-white hover:bg-gray-400 text-neutral">Details</button>
+                        <button className="btn ml-5 border-transparent hover:border-transparent bg-neutral hover:bg-secondary text-background">Details</button>
                       </Link>
                     </div>
                 </div>
