b7019789df29883d4589550db098b9750ab64421
lschrays <lschrays@smail.uni-koeln.de>
2022-11-30 15:33:51 +0100

    Add database connection

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,15 +1,17 @@
 import formidable from "formidable";
 import fs from "fs";
+const mysql = require("mysql");
+const fastcsv = require("fast-csv");
 
 export const config = {
   api: {
-    bodyParser: false
-  }
+    bodyParser: false,
+  },
 };
 
 const post = async (req, res) => {
   const form = new formidable.IncomingForm();
-  form.parse(req, async function (err, fields, files) {
+  form.parse(req, async function(err, fields, files) {
     await saveFile(files.file);
     return res.status(201).send("");
   });
@@ -17,7 +19,43 @@ const post = async (req, res) => {
 
 const saveFile = async (file) => {
   const data = fs.readFileSync(file.filepath);
+  //save file locally
   fs.writeFileSync(`./public/testUpload.csv`, data);
+  //save file on db
+  let stream = fs.createReadStream(data);
+  let csvData = [];
+  let csvStream = fastcsv
+    .parse({ delimiter: ";" })
+    .on("data", function(data) {
+      csvData.push(data);
+    })
+    .on("end", function() {
+      // remove the first line: header
+      csvData.shift();
+
+      // create a new connection to the database
+      const connection = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "@UniKoeln123",
+        database: "test_db",
+      });
+
+      // open the connection
+      connection.connect((error) => {
+        if (error) {
+          console.error(error);
+        } else {
+          let query =
+            "INSERT INTO mytable (lfdNr, Gruppe, Platz, Matrikelnummer,Abschlussziel,SPOVersion,StudienID,Studium,Fachsemester,Anmeldedatum,Kennzahl) VALUES ?";
+          connection.query(query, [csvData], (error, response) => {
+            console.log(error || response);
+          });
+        }
+      });
+    });
+
+  stream.pipe(csvStream);
   await fs.unlinkSync(file.filepath);
   return;
 };
