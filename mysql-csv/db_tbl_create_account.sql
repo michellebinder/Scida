c9086fead3b589ddd44fe1ba412efcc7e0d0154b
Jingwen Yi <jyi@smail.uni-koeln.de>
2022-11-30 21:12:29 +0100

    Sql scripts for tables: accounts, attendance and blocks

--- mysql-csv/db_tbl_create_account.sql
+++ mysql-csv/db_tbl_create_account.sql
@@ -5,21 +5,22 @@
 /*choose the database we will use*/
 USE test_db;
 /*delete the existed table "account" cause we need to cahnge its structure*/
-DROP TABLE account; 
+DROP TABLE accounts; 
 /*automatic generated id and password*/
-CREATE TABLE account(
-   lecturer_id        INTEGER  NOT NULL AUTO_INCREMENT,
+CREATE TABLE accounts(
+   account_id        INTEGER  NOT NULL AUTO_INCREMENT,
    first_name         VARCHAR(30) NOT NULL
   ,last_name          VARCHAR(30) NOT NULL
   ,email              VARCHAR(30) NOT NULL
+  ,account_pwd       VARCHAR(80) NOT NULL
+  ,salt               VARCHAR(50) NOT NULL
   ,account_role       VARCHAR(20) NOT NULL
-  /* ,lecturer_pwd       VARCHAR(30) NOT NULL */
-  ,PRIMARY KEY (lecturer_id)
+  ,PRIMARY KEY (account_id)
 );
 
 
 /*see the structure of this table*/
-SHOW COLUMNS FROM test_db.account;
+SHOW COLUMNS FROM test_db.accounts;
 
 /*not necessary, but if you want see the contents (though the table is empty rn, so it will only return "Empty set") */
-SELECT * FROM test_db.account;
+SELECT * FROM test_db.accounts;
---
+++ mysql-csv/db_tbl_create_attendance.sql
@@ -0,0 +1,23 @@
+/*run it in mysql CLI using ' source ~/Documents/sci/scida/mysql-csv/db_tbl_create_attendance.sql ' or something similar, it depends on the path*7
+/*we don't need this, cause test_db is existed already */
+/*CREATE DATABASE test_db;*/
+
+/*choose the database we will use*/
+USE test_db;
+/*delete the existed table "account" cause we need to cahnge its structure, don't need it if we don't have such a table*/
+DROP TABLE attendance; 
+/*I choose username and date of attendance cause a student cannot attand the same session twice or more, and we should avoid that*/
+CREATE TABLE attendance(
+   block_id           INTEGER  NOT NULL 
+  ,student_username   VARCHAR(30) NOT NULL
+  ,lecturer_id        VARCHAR(30) NOT NULL
+  ,confirmed_at       DATE
+  ,PRIMARY KEY (student_username, confirmed_at)    
+);
+
+
+/*see the structure of this table*/
+SHOW COLUMNS FROM test_db.attendance;
+
+/*not necessary, but if you want see the contents (though the table is empty rn, so it will only return "Empty set") */
+SELECT * FROM test_db.attendance;
