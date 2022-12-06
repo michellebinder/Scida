b46e3110f1488756253b42a7c9a40b967736a3d7
Jingwen Yi <jyi@smail.uni-koeln.de>
2022-12-06 16:04:27 +0100

    get detailed info of a praktika

--- mysql/tbl_insert_fake_block.sql
+++ mysql/tbl_insert_fake_block.sql
@@ -4,20 +4,4 @@ INSERT INTO blocks (block_name,block_id,group_id,lecturer_id,date_start,date_end
 INSERT INTO blocks (block_name,block_id,group_id,lecturer_id,date_start,date_end) value ('Innere Medizin','1234','08','admin5@admin','2022-10-17','2022-10-21');
 INSERT INTO blocks (block_name,block_id,group_id,lecturer_id,date_start,date_end) value ('Pädiatrie','4567','07','admin6@admin','2022-11-01','2022-11-05');
 
-SELECT 
-blocks.block_name,blocks.block_id,blocks.group_id 
-FROM blocks 
-INNER JOIN mytable_fake
-ON 
-blocks.block_name = mytable_fake.Block_name AND blocks.group_id = mytable_fake.Gruppe WHERE mytable_fake.Matrikelnummer = '5558107';
 
-
-
-SELECT 
-blocks.block_name,blocks.group_id,attendance.*
-FROM blocks
-INNER JOIN attendance
-ON blocks.block_id = attendance.block_id AND attendance.student_username = 'mmuster'
-INNER JOIN mytable_fake
-ON 
-blocks.block_name = mytable_fake.Block_name AND blocks.group_id = mytable_fake.Gruppe AND mytable_fake.Matrikelnummer = '5558107';
--- pages/api/getTimetable.js
+++ pages/api/getTimetable.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 const blockname = [];
-    const groupnumber = [];
-    const blockIDQuery = [];
-    const blockID = [];
-    const sqlQuery = ("SELECT blocks.block_name,blocks.group_id,blocks.date_start,blocks.date_end,attendance.* FROM blocks INNER JOIN attendance ON blocks.block_id = attendance.block_id AND attendance.student_username = ? INNER JOIN mytable_fake ON blocks.block_name = mytable_fake.Block_name AND blocks.group_id = mytable_fake.Gruppe AND mytable_fake.Matrikelnummer = ?;")
+const groupnumber = [];
+const blockIDQuery = [];
+const blockID = [];
+
 export default function handler(req, res) {
     const connection = mysql.createConnection({
         host: "127.0.0.1",
@@ -16,10 +16,11 @@ export default function handler(req, res) {
     const groupnumber = [];
     const blockIDQuery = [];
     const blockID = []; */
+    const sqlQuery = "SELECT blocks.block_name,blocks.group_id,timetable.* FROM blocks INNER JOIN timetable ON blocks.block_id = timetable.block_id WHERE blocks.block_id = ?";
 
     connection.connect(function (err) {
         if (err) throw err;
-        connection.query(sqlQuery, ['mmuster','5558107'], function (err, results, fields) {
+        connection.query(sqlQuery, ['0123'], function (err, results, fields) {
             if (err) throw err;
             res.status(200).json(results);
             console.log(results);
