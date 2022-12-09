42127d8b8dbfd6afd7b0f01b910e61584f038b0b
Jingwen Yi <jyi@smail.uni-koeln.de>
2022-12-09 19:16:56 +0100

    Transformation of csv file

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -14,7 +14,7 @@ export const config = {
 //Process post request
 const post = async (req, res) => {
   const form = new formidable.IncomingForm();
-  form.parse(req, async function(err, fields, files) {
+  form.parse(req, async function (err, fields, files) {
     await saveFile(files.file);
     return res.status(201).send("");
   });
@@ -31,12 +31,33 @@ const saveFile = async (file) => {
   let csvData = [];
   let csvStream = fastcsv
     .parse({ delimiter: ";" })
-    .on("data", function(data) {
+    .on("data", function (data) {
       csvData.push(data);
     })
-    .on("end", function() {
+    .on("end", function () {
       //Remove the first line: header
       csvData.shift();
+      // transformation of data structure (split up blockname and group id)
+      const csvlength1 = csvData.length;
+      const csvlength2 = csvData[0].length;
+      for (let i = 0; i < csvlength1; i++) {
+        for (let j = csvlength2; j >= 0; j--) {
+          if (j == 0) {
+            //nothing changes
+          }
+          else if (j == 1) {
+            csvData[i][j] = csvData[i][j].substring(0, csvData[i][j].length - 10);
+
+          } else if (j == 2) {
+            csvData[i][j] = csvData[i][j - 1].substring(csvData[i][j - 1].length - 2, csvData[i][j - 1].length);
+
+          } else {
+            csvData[i][j] = csvData[i][j - 1];
+
+          }
+        }
+      }
+      console.log(csvData);
 
       //Create a new connection to the database
       const connection = mysql.createConnection({
@@ -52,7 +73,7 @@ const saveFile = async (file) => {
           console.error(error);
         } else {
           let query =
-            "INSERT INTO mytable (lfdNr, Gruppe, Platz, Matrikelnummer,Abschlussziel,SPOVersion,StudienID,Studium,Fachsemester,Anmeldedatum,Kennzahl) VALUES ?";
+            "INSERT INTO mytable (lfdNr, Block_name, Gruppe, Platz, Matrikelnummer,Abschlussziel,SPOVersion,StudienID,Studium,Fachsemester,Anmeldedatum,Kennzahl) VALUES ?";
           connection.query(query, [csvData], (error, response) => {
             console.log(error || response);
           });
@@ -65,7 +86,7 @@ const saveFile = async (file) => {
 
   stream.pipe(csvStream);
 
-  
+
   return;
 };
 
@@ -82,10 +103,10 @@ export default (req, res) => {
   req.method === "POST"
     ? post(req, res)
     : req.method === "PUT"
-    ? console.log("PUT")
-    : req.method === "DELETE"
-    ? console.log("DELETE")
-    : req.method === "GET"
-    ? console.log("GET")
-    : res.status(404).send("");
+      ? console.log("PUT")
+      : req.method === "DELETE"
+        ? console.log("DELETE")
+        : req.method === "GET"
+          ? console.log("GET")
+          : res.status(404).send("");
 };
