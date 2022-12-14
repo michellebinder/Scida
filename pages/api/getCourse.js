7526c6a389d4ca9c42d44a27c2899ff7e49f2555
lschrays <lschrays@smail.uni-koeln.de>
2022-12-14 14:22:40 +0100

    removed code comments

--- pages/api/getCourse.js
+++ pages/api/getCourse.js
@@ -6,17 +6,6 @@ export default function handler(req, res) {
   const sqlQuery =
     "SELECT blocks.block_name,blocks.group_id,blocks.date_start,blocks.date_end,attendance.* FROM blocks INNER JOIN attendance ON blocks.block_id = attendance.block_id AND attendance.student_username = ? INNER JOIN mytable_fake ON blocks.block_name = mytable_fake.Block_name AND blocks.group_id = mytable_fake.Gruppe AND mytable_fake.Matrikelnummer = ?;";
 
-  /* if (!req.body) {
-        // Sends a HTTP bad request error code
-        console.log("Something wrong");
-        return res.status(400).json({ data: "Something wrong" });
-      } */
-
-  /*weird but works */
-
-  /*  const usr= req.body.stud.stud_username;
-    const matri= req.body.stud.stud_matrikel; */
-
   const connection = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
@@ -33,20 +22,10 @@ export default function handler(req, res) {
       function(err, results, fields) {
         if (err) throw err;
 
-        /*  res.status(200).json(results); */
-        /* res.status(200).json(`datareceived`); */
-
         console.log(results.length);
-        /* const data = [];
-            for(let i = 0; i<results.length; i++){
-                data.push(results[i]);
-            }
-            console.log(data); */
 
         let dataString = JSON.stringify(results);
 
-        console.log(dataString);
-        /* res.status(200).json(data); */
         res.status(200).json(`${dataString}`);
       }
     );
