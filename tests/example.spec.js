ba467c82eb9c5f9fb2d75ea0858e5773b218722f
Michelle Binder <mbinder1@smail.uni-koeln.de>
2022-12-14 22:12:43 +0100

    Add test which checks whether logo is visible

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,9 +1,23 @@
 // @ts-check
 const { test, expect } = require("@playwright/test");
 
-test("homepage has title and links to intro page", async ({ page }) => {
+test("navbar contains title and links to intro page", async ({ page }) => {
   await page.goto("/");
   const navbar = page.locator("data-testid=navbar");
 
   await expect(navbar).toContainText("Scida");
+  await expect(navbar).toContainText("Medizinische Fakultät - Universität zu Köln");
 });
+
+test("navbar contains logo", async ({ page }) => {
+  await page.goto("/");
+  const navbar = page.locator("data-testid=img");
+  await page.isVisible("img")
+});
+
+// test("navbar contains link to intro page", async ({ page }) => {
+//   await page.goto("/");
+//   const navbar = page.locator("data-testid=navbar");
+
+//   await expect(navbar).toContainText("Scida");
+// });
