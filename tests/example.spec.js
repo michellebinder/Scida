073fa4ffbfaed764265ac4e2fd78299e94af57a3
Michelle Binder <mbinder1@smail.uni-koeln.de>
2022-12-15 11:42:34 +0100

    Add test for footer of admin dashboard

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -142,3 +142,14 @@ test('admin dashboard contains the right text and links to the corresponding pag
   //await page.getByRole('link', { name: 'Ausdrucke Lade dir hier Anwesenheitslisten herunter.' }).click();
 });
 
+test('admin dashboard footer contains the right text and links to the corresponding pages', async ({ page }) => {
+  await page.goto('http://localhost:3000/dashboardAdmin/');
+  await page.getByRole('contentinfo').click();
+  await page.getByText('© 2022 Scida. Alle Rechte vorbehalten.').click();
+  await page.getByText('|').first().click();
+  await page.getByRole('link', { name: 'Support' }).click();
+  await page.getByText('|').nth(1).click();
+  await page.getByRole('link', { name: 'Datenschutz' }).click();
+  await page.getByText('|').nth(2).click();
+  await page.getByRole('link', { name: 'Impressum' }).click();
+});
