6f5fa77b10ccd84b8ec385ad472fbb0d83e949df
Michelle Binder <mbinder1@smail.uni-koeln.de>
2022-12-15 10:59:43 +0100

    Fix bug in footer test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,32 +1,24 @@
 // @ts-check
 const { test, expect } = require("@playwright/test");
 
-test("navbar contains title and links to intro page", async ({ page }) => {
-  await page.goto("/");
-  const navbar = page.locator("data-testid=navbar");
+ {/* LOGIN-PAGE TESTS */}
 
-  await expect(navbar).toContainText("Scida");
-  await expect(navbar).toContainText("Medizinische Fakultät - Universität zu Köln");
+test('navbar contains title, logo and links to intro page', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('img').click();
+  await page.getByRole('link', { name: 'Scida Medizinische Fakultät - Universität zu Köln' }).click();
+  await page.getByRole('img').click();
 });
 
-test("navbar contains logo", async ({ page }) => {
-  await page.goto("/");
-  const navbar = page.locator("data-testid=img");
-  await page.isVisible("img")
+test('footer contains correct text and links to corresponding pages', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('contentinfo').click();
+  await page.getByText('© 2022 Scida. Alle Rechte vorbehalten.').click();
+  await page.getByText('|').first().click();
+  await page.getByRole('link', { name: 'Support' }).click();
+  await page.getByText('|').nth(1).click();
+  await page.getByRole('link', { name: 'Datenschutz' }).click();
+  await page.getByText('|').nth(2).click();
+  await page.getByRole('link', { name: 'Impressum' }).click();
 });
 
-test("footer contains right text and links to corresponding pages", async ({ page }) => {
-
-  // TODO: fix text check (at the moment they somehow pass with any text)
-  page.locator('footer', { hasText: '© 2022 Scida.' });
-  page.locator('footer', { hasText: 'Alle Rechte vorbehalten.' });
-  
-  page.locator('footer', { hasText: 'Support' });
-  await page.goto('/support');
-  
-  page.locator('footer', { hasText: 'Datenschutz' });
-  await page.goto('/datenschutz');
-
-  page.locator('footer', { hasText: 'Impressum' });
-  await page.goto('/impressum');
-});
