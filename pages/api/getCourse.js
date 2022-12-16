d0d44721f1c354e3a84d6a63498969adcd1d678a
Marc Peter <mpeter6@smail.uni-koeln.de>
2022-12-16 13:06:16 +0100

    Fix merging issues

--- pages/api/getCourse.js
+++ pages/api/getCourse.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 
 export default function handler(req, res) {
   const sqlQuery =
-    "SELECT blocks.block_name,blocks.group_id,blocks.date_start,blocks.date_end,attendance.* FROM blocks INNER JOIN attendance ON blocks.block_id = attendance.block_id AND attendance.student_username = ? INNER JOIN mytable_fake ON blocks.block_name = mytable_fake.Block_name AND blocks.group_id = mytable_fake.Gruppe AND mytable_fake.Matrikelnummer = ?;";
+    "SELECT blocks.block_name,blocks.group_id,blocks.date_start,blocks.date_end,attendance.* FROM blocks INNER JOIN attendance ON blocks.block_id = attendance.block_id AND attendance.student_username = ? INNER JOIN mytable ON blocks.block_name = mytable.Block_name AND blocks.group_id = mytable.Gruppe AND mytable.Matrikelnummer = ?;";
 
   const connection = mysql.createConnection({
     host: "127.0.0.1",
