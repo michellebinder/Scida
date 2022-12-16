7dfc2e3ca7738c0767aed1a255ce3b3760df0720
Jingwen Yi <jyi@smail.uni-koeln.de>
2022-12-16 13:04:45 +0100

    Update getCourse

--- pages/api/getCourse.js
+++ pages/api/getCourse.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 
 export default function handler(req, res) {
   const sqlQuery =
-    "SELECT blocks.block_name,blocks.group_id,blocks.date_start,blocks.date_end,attendance.* FROM blocks INNER JOIN attendance ON blocks.block_id = attendance.block_id AND attendance.student_username = ? INNER JOIN mytable_fake ON blocks.block_name = mytable_fake.Block_name AND blocks.group_id = mytable_fake.Gruppe AND mytable_fake.Matrikelnummer = ?;";
+    "SELECT blocks.block_name,blocks.group_id,blocks.date_start,blocks.date_end,attendance.*,timetable.sess_id,timetable.sess_type,timetable.sess_time FROM blocks INNER JOIN attendance  ON blocks.block_id = attendance.block_id AND attendance.student_username = ? LEFT JOIN timetable ON timetable.block_id = blocks.block_id AND timetable.sess_id = attendance.sess_id INNER JOIN mytable_fake ON  blocks.block_name = mytable_fake.Block_name AND blocks.group_id = mytable_fake.Gruppe AND mytable_fake.Matrikelnummer = ?";
 
   const connection = mysql.createConnection({
     host: "127.0.0.1",
