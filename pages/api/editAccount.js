d01e91e6e00f7f6c8d72789a3e4f437d01c378da
Marc Peter <mpeter6@smail.uni-koeln.de>
2022-12-26 23:17:04 +0100

    Adds Fail response

--- pages/api/editAccount.js
+++ pages/api/editAccount.js
@@ -4,37 +4,40 @@ const querystring = require("querystring");
 const mysql = require("mysql");
 
 export default function handler(req, res) {
-    // Get data submitted in request's body.
-    const body = req.body;
-  
-    const id = body.editId;
-    const firstName = body.editFirstName;
-    const lastName = body.editLastName;
-    const email = body.editEmail;
-    const role = body.editRole;
+  // Get data submitted in request's body.
+  const body = req.body;
 
-    //database information
-    const connection = mysql.createConnection({
-      host: "127.0.0.1",
-      user: "root",
-      password: "@UniKoeln123",
-      port: 3306,
-      database: "test_db",
-    });
-    //connect database
-    connection.connect();
-    //content query
-    connection.query(
-      "UPDATE accounts SET first_name=?, last_name=?, email=?, account_role=? WHERE account_id=?",
-      [firstName, lastName, email, role, id],
-      (err, results, fields) => {
-        //error
-        res.status(200).json(`${results}`);
-        if (err) throw err;
-        res.end();
+  const id = body.editId;
+  const firstName = body.editFirstName;
+  const lastName = body.editLastName;
+  const email = body.editEmail;
+  const role = body.editRole;
+
+  //database information
+  const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "@UniKoeln123",
+    port: 3306,
+    database: "test_db",
+  });
+  //connect database
+  connection.connect();
+  //content query
+  connection.query(
+    "UPDATE accounts SET first_name=?, last_name=?, email=?, account_role=? WHERE account_id=?",
+    [firstName, lastName, email, role, id],
+    (err, results, fields) => {
+      //error
+      res.status(200).json(`${results}`);
+      if (err) {
+        res.status(200).json(`FAIL`);
+      }
+      if (err) throw err;
+      res.end();
     }
-    );
+  );
 
-    // disconnect database
-    connection.end();
-  }
+  // disconnect database
+  connection.end();
+}
