17edc9bca2a85aa8856fc2e67f5a22e6d021e466
lschrays <lschrays@smail.uni-koeln.de>
2022-12-26 15:46:18 +0100

    Add html form element to allow keyboard enter

--- components/login.js
+++ components/login.js
@@ -301,74 +301,71 @@ export default function Login({ type = "" }) {
     <div>
       <div className="card rounded-lg flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 dark:bg-neutral">
         <div className="card-body pb-1">
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Benutzername / Email</span>
-            </label>
-            <input
-              type="text"
-              value={email}
-              placeholder="mmuster1 / mmuster1@test.de "
-              className="input input-bordered"
-              onChange={(e) => createEmail(e.target.value)}
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Passwort</span>
-            </label>
-            <input
-              type="password"
-              value={password}
-              placeholder="Passwort"
-              className="input input-bordered"
-              onChange={(e) => createPassword(e.target.value)}
-            />
-            <label className="label">
-              <a
-                href="https://kim.uni-koeln.de"
-                className="label-text-alt link link-hover"
-              >
-                Passwort vergessen?
-              </a>
-            </label>
-          </div>
-          <div className="form-control mt-6">
-            <button
-              onClick={handleSubmitCombined}
-              className="btn btn-primary"
-              disabled={busy}
-            >
-              Einloggen
-            </button>
-            {/* Invisible alert to prevent the login form to wobble arround */}
-            <div className={alertVisibility1}>
-              <div className="alert alert-error bg-transparent h-1 mt-1">
-                <div></div>
-              </div>
+          {/* Adding a html form element to allow keyboard event to click "Einloggen" button */}
+          <form onSubmit={handleSubmitCombined}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Benutzername / Email</span>
+              </label>
+              <input
+                value={email}
+                placeholder="mmuster1 / mmuster1@test.de "
+                className="input input-bordered"
+                onChange={(e) => createEmail(e.target.value)}
+              />
             </div>
-            {/* Actual error alert */}
-            <div className={alertVisibility2}>
-              <div className="alert alert-error shadow-lg h-1 mt-1">
-                <div>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="stroke-current flex-shrink-0 h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
-                  <span>{error}</span>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Passwort</span>
+              </label>
+              <input
+                value={password}
+                placeholder="Passwort"
+                className="input input-bordered"
+                onChange={(e) => createPassword(e.target.value)}
+              />
+              <label className="label">
+                <a
+                  href="https://kim.uni-koeln.de"
+                  className="label-text-alt link link-hover"
+                >
+                  Passwort vergessen?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button type="submit" className="btn btn-primary" disabled={busy}>
+                Einloggen
+              </button>
+              {/* Invisible alert to prevent the login form to wobble arround */}
+              <div className={alertVisibility1}>
+                <div className="alert alert-error bg-transparent h-1 mt-1">
+                  <div></div>
+                </div>
+              </div>
+              {/* Actual error alert */}
+              <div className={alertVisibility2}>
+                <div className="alert alert-error shadow-lg h-1 mt-1">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="stroke-current flex-shrink-0 h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                    <span>{error}</span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
