395d0c16e0958cb83be4b82ea1c17942ef9b1930
Jonas <jonas.wetzel23@gmail.com>
2022-12-30 13:09:58 +0100

    Login tests for dozenten and dekanat accounts

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -52,6 +52,26 @@ test('login with student test account', async ({ page }) => {
   await page.getByRole('button', { name: 'Einloggen' }).click();
   await page.getByRole('heading', { name: 'Hallo Studierende!' }).click();
   await page.getByRole('button', { name: 'Logout' }).click();
-  
+});
 
+test('login with dozent test account', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByPlaceholder('mmuster1 / mmuster1@test.de ').click();
+  await page.getByPlaceholder('mmuster1 / mmuster1@test.de ').type('dozierende@test.de');
+  await page.getByPlaceholder('Passwort').click();
+  await page.getByPlaceholder('Passwort').type('123test');
+  await page.getByRole('button', { name: 'Einloggen' }).click();
+  await page.getByRole('heading', { name: 'Hallo Dozierende!' }).click();
+  await page.getByRole('button', { name: 'Logout' }).click();
+});
+
+test('login with dekanat test account', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByPlaceholder('mmuster1 / mmuster1@test.de ').click();
+  await page.getByPlaceholder('mmuster1 / mmuster1@test.de ').type('dekanat@test.de');
+  await page.getByPlaceholder('Passwort').click();
+  await page.getByPlaceholder('Passwort').type('123test');
+  await page.getByRole('button', { name: 'Einloggen' }).click();
+  await page.getByRole('heading', { name: 'Hallo Dekanat!' }).click();
+  await page.getByRole('button', { name: 'Logout' }).click();
 });
