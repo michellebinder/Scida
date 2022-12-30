864bbd81d586efd1973b3b1010f1882842ac24d7
feli.pn <fpreussn@smail.uni-koeln.de>
2022-12-30 16:40:16 +0100

    Unify participantsList for lect and admin

--- pages/participantsLecturer.js
+++ pages/participantsLecturer.js
@@ -136,5 +136,77 @@ export default function Home() {
         </div>
       </>
     );
+  } else if (role === "B" || role === "A") {
+    return (
+      <>
+        <Head>
+          <title>Scida</title>
+          <meta charSet="utf-8" />
+        </Head>
+        {/* Div that stretches from the very top to the very bottom */}
+        <div className="flex flex-col h-screen justify-between bg-base-100">
+          {/* Dashboard navbar with navigation items  */}
+          <Navbar></Navbar>
+          <div className="flex flex-row grow">
+            {/* Sidebar only visible on large screens */}
+            <Sidebar type="lecturer"></Sidebar>
+            <div className="hero grow">
+              {/* Grid for layouting welcome text and card components, already responsive */}
+              <div className="grid hero-content text-center text-neutral lg:p-10">
+                <div className="text-secondary dark:text-white">
+                  {/* display courseID as determined by href url */}
+                  <h1 className="mb-5 text-5xl font-bold text-center">
+                    {/* TODO: backend: find out and display course name not courseID */}
+                    {courseName}
+                  </h1>
+                  <h1 className="mb-5 text-3xl font-bold text-center">
+                    {/* TODO: frontend: pass chosen group number to this page and display here */}
+                    Teilnehmerliste
+                  </h1>
+                </div>
+                <div class="overflow-auto">
+                  {/* display table component with attendance details for the course */}
+                  <div className="grid w-fit sm:grid-cols-1 gap-5">
+                    {/* TODO: backend: find out corresponding values for course and pass to courseDate */}
+                    <div class="container mx-auto">
+                      <div class="overflow-auto">
+                        <table class="table table-normal w-full text-primary text-center dark:text-white">
+                          <thead>
+                            <tr>
+                              <th></th>
+                              <th>Kürzel</th>
+                              <th>Matrikelnr.</th>
+                              <th>Anwesenheit</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {matrikel.map((matr, index) => (
+                              <tr class="hover">
+                                <td>{index + 1}</td>
+                                <td>mmuster1</td>
+                                <td>{matr.matr}</td>
+                                <td>
+                                  <input
+                                    type="checkbox"
+                                    class="checkbox checkbox-primary"
+                                    checked={matr.checked}
+                                    onClick={() => handleClick(index)}
+                                  />
+                                </td>
+                              </tr>
+                            ))}
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Footer></Footer>
+        </div>
+      </>
+    );
   }
 }
