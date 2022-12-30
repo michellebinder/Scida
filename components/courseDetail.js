d6f8015fcace5ed638bbe747ddd1cc0d15472ec9
Michelle Binder <mbinder1@smail.uni-koeln.de>
2022-12-30 22:51:29 +0100

    Add button for adding new groups to a praktikum

--- components/courseDetail.js
+++ components/courseDetail.js
@@ -64,6 +64,20 @@ export default function CourseDetail({
                   <Accordion title="Gruppe 3">
                     <CourseTable praktID="2462" type="admin"></CourseTable>
                   </Accordion>
+                  <button className="w-full bg-primary bg-opacity-20 rounded-md shadow-lg px-4 py-2 font-bold text-primary group flex items-center focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-primary transition duration-150 ease-in-out">
+                    <svg
+                      class="svg-icon stroke-primary mr-3"
+                      viewBox="0 0 20 20"
+                      width="18"
+                      height="18"
+                    >
+                      <path
+                        fill="none"
+                        d="M13.388,9.624h-3.011v-3.01c0-0.208-0.168-0.377-0.376-0.377S9.624,6.405,9.624,6.613v3.01H6.613c-0.208,0-0.376,0.168-0.376,0.376s0.168,0.376,0.376,0.376h3.011v3.01c0,0.208,0.168,0.378,0.376,0.378s0.376-0.17,0.376-0.378v-3.01h3.011c0.207,0,0.377-0.168,0.377-0.376S13.595,9.624,13.388,9.624z M10,1.344c-4.781,0-8.656,3.875-8.656,8.656c0,4.781,3.875,8.656,8.656,8.656c4.781,0,8.656-3.875,8.656-8.656C18.656,5.219,14.781,1.344,10,1.344z M10,17.903c-4.365,0-7.904-3.538-7.904-7.903S5.635,2.096,10,2.096S17.903,5.635,17.903,10S14.365,17.903,10,17.903z"
+                      ></path>
+                    </svg>
+                    Neue Gruppe hinzufügen
+                  </button>
                 </div>
               </div>
             </div>
