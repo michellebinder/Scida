b2d158343eabb13b928b1d641da4e52a082b746a
Michelle Binder <mbinder1@smail.uni-koeln.de>
2023-01-03 12:16:10 +0100

    Add button for deleting a group

--- components/courseDetail.js
+++ components/courseDetail.js
@@ -97,11 +97,10 @@ export default function CourseDetail({
                   <div className="text-secondary dark:text-white">
                     {/* display courseID as determined by href url */}
                     <h1 className="mb-5 text-5xl font-bold text-center">
-                      {/* TODO: backend: find out and display course name not courseID */}
                       {courseName}
                     </h1>
                     <h1 className="mb-5 text-3xl font-bold text-center">
-                      {/* TODO: frontend: pass chosen group number to this page and display here */}
+                      {/* TODO: maybe remove since it's not necessary for the users to see the ID? */}
                       (ID: {blockId})
                     </h1>
                     <h1 className="mb-5 text-3xl font-bold text-center">
@@ -110,6 +109,7 @@ export default function CourseDetail({
                     </h1>
                   </div>
                   <div>
+                    {/* TODO: find out whether part below can be deleted */}
                     {/* display table component with attendance details for the course */}
                     {/* <div className="grid w-fit sm:grid-cols-1 gap-5">
                     {/* TODO: backend: find out corresponding values for course and pass to courseDate */}
@@ -118,7 +118,6 @@ export default function CourseDetail({
                   </div>
                   <div className="grid gap-y-5">
                     {/* Collapsible section which contains all the groups of the current Praktikum */}
-                    {/* TODO backend: add as many Accordions as there are groups in the current Praktikum */}
                     {accordions.map((accordion, index) => (
                       <Accordion
                         group={handleGroup}
