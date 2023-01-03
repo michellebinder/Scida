144938907227e8e037a1de34fd405534101910c6
lschrays <lschrays@smail.uni-koeln.de>
2023-01-03 19:59:03 +0100

    Add type to popup to control its color

--- components/popUp.js
+++ components/popUp.js
@@ -1,21 +1,33 @@
 import { parseStream } from "fast-csv";
 import React, { useState } from "react";
+import { blueGray } from "tailwindcss/colors";
 
 export default function PopUp({
   text = "",
   password = "",
   btnEnabled = false,
+  type = "", //success, error or blank for different colors
 }) {
   btnEnabled = password != ""; //only if password exist, copy password activated
   const copyPassword = () => {
     navigator.clipboard.writeText(password);
   };
+
+  //Set color depending on type
+  const bgColor = "";
+  if (type == "success") {
+    bgColor = "bg-success p-10 rounded-2xl flex flex-col gap-4";
+  } else if (type == "error") {
+    bgColor = "bg-error p-10 rounded-2xl flex flex-col gap-4";
+  } else {
+    bgColor = "bg-gray-800 p-10 rounded-2xl flex flex-col gap-4";
+  }
   return (
     <>
       <div className="fixed z-10 left-0 right-0 top-0 bottom-0 m-auto flex flex-col justify-around">
         <div className="flex justify-center">
           <div className="flex flex-col justify-center">
-            <div className="bg-gray-800 p-10 rounded-2xl flex flex-col gap-4">
+            <div className={bgColor}>
               <h1>{text}</h1>
               {btnEnabled && (
                 <>
