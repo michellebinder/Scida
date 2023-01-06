5ce151ae555c66c9dd8544a72584969b67cdb941
Jonas <jonas.wetzel23@gmail.com>
2023-01-06 14:22:19 +0100

    Delete openVPN and try tunneling different

--- tests/loginAccounts.spec.js
+++ tests/loginAccounts.spec.js
@@ -6,7 +6,9 @@ const { test, expect } = require("@playwright/test");
   /* LOGIN-ACCOUNTS TESTS */
 }
 test('login with student test account', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('https://2793-134-95-7-150.eu.ngrok.io/');
+  await page.getByRole('button', { name: 'Visit Site' }).click();
+  await page.goto('https://2793-134-95-7-150.eu.ngrok.io/');
   await page.getByPlaceholder('mmuster1 / mmuster1@test.de ').click();
   await page.getByPlaceholder('mmuster1 / mmuster1@test.de ').type('studierende@test.de');
   await page.getByPlaceholder('Passwort').click();
@@ -17,7 +19,9 @@ test('login with student test account', async ({ page }) => {
 });
 
 test('login with dozent test account', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('https://2793-134-95-7-150.eu.ngrok.io/');
+  await page.getByRole('button', { name: 'Visit Site' }).click();
+  await page.goto('https://2793-134-95-7-150.eu.ngrok.io/');
   await page.getByPlaceholder('mmuster1 / mmuster1@test.de ').click();
   await page.getByPlaceholder('mmuster1 / mmuster1@test.de ').type('dozierende@test.de');
   await page.getByPlaceholder('Passwort').click();
@@ -28,7 +32,7 @@ test('login with dozent test account', async ({ page }) => {
 });
 
 test('login with dekanat test account', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('https://2793-134-95-7-150.eu.ngrok.io/');
   await page.getByPlaceholder('mmuster1 / mmuster1@test.de ').click();
   await page.getByPlaceholder('mmuster1 / mmuster1@test.de ').type('dekanat@test.de');
   await page.getByPlaceholder('Passwort').click();
--- tests/loginPage.spec.js
+++ tests/loginPage.spec.js
@@ -9,7 +9,9 @@ const { test, expect } = require("@playwright/test");
 test("navbar contains title, logo and links to intro page", async ({
   page,
 }) => {
-  await page.goto("http://localhost:3000/");
+  await page.goto('https://2793-134-95-7-150.eu.ngrok.io/');
+  await page.getByRole('button', { name: 'Visit Site' }).click();
+  await page.goto("https://2793-134-95-7-150.eu.ngrok.io/");
   await page.getByRole("img").click();
   await page.getByRole("link", {name: "Scida Medizinische Fakultät - Universität zu Köln",}).click();
   await page.getByTestId('navbar').click();
@@ -18,7 +20,9 @@ test("navbar contains title, logo and links to intro page", async ({
 test("footer contains correct text and links to corresponding pages", async ({
   page,
 }) => {
-  await page.goto("http://localhost:3000/");
+  await page.goto('https://2793-134-95-7-150.eu.ngrok.io/');
+  await page.getByRole('button', { name: 'Visit Site' }).click();
+  await page.goto("https://2793-134-95-7-150.eu.ngrok.io/");
   await page.getByRole("contentinfo").click();
   await page.getByText("© 2022 Scida. Alle Rechte vorbehalten.").click();
   await page.getByText("|").first().click();
@@ -32,12 +36,14 @@ test("footer contains correct text and links to corresponding pages", async ({
 test("login window contains all components", async ({
     page,
   }) => {
-    await page.goto("http://localhost:3000/");
+    await page.goto('https://2793-134-95-7-150.eu.ngrok.io/');
+    await page.getByRole('button', { name: 'Visit Site' }).click();
+    await page.goto("https://2793-134-95-7-150.eu.ngrok.io/");
     await page.getByText('Benutzername / Email').click();
     await page.getByPlaceholder('mmuster1 / mmuster1@test.de ').click();
     await page.getByPlaceholder('Passwort').click();
     await page.getByRole('link', { name: 'Passwort vergessen?' }).click();
-    await page.goto("http://localhost:3000/");
+    await page.goto("https://2793-134-95-7-150.eu.ngrok.io/");
     await page.getByRole('button', { name: 'Einloggen' }).click();
     await page.getByText('Zugangsdaten falsch').click();
   });
