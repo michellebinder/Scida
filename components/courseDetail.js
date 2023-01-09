fd9048cb5ed34f723bdd8ccf6fe72f8e2bf4a46f
Michelle Binder <mbinder1@smail.uni-koeln.de>
2023-01-09 05:56:18 +0000

    Remove console.logs and unnecessary comments

--- components/courseDetail.js
+++ components/courseDetail.js
@@ -24,7 +24,6 @@ export default function CourseDetail({
         groups.push(row.group_id);
       });
       groups = remove_duplicates(groups);
-      console.log(data);
       groups.map((row) => {
         res.push({
           title: row,
@@ -38,7 +37,6 @@ export default function CourseDetail({
           ),
         });
       });
-      console.log(res);
 
       const [accordions, setAccordions] = useState(res);
       const [grouplist, setGrouplist] = useState(groups);
@@ -69,7 +67,6 @@ export default function CourseDetail({
           sess_type: undefined,
         };
         data.push(emptyRow);
-        console.log(groups);
         setAccordions([
           ...accordions,
           {
@@ -117,11 +114,6 @@ export default function CourseDetail({
                       {courseName}
                     </h1>
                     <h1 className="mb-5 text-3xl font-bold text-center">
-                      {/* TODO: maybe remove since it's not necessary for the users to see the ID? */}
-                      (ID: {blockId})
-                    </h1>
-                    <h1 className="mb-5 text-3xl font-bold text-center">
-                      {/* TODO: frontend: pass chosen group number to this page and display here */}
                       {selectedValue}
                     </h1>
                   </div>
@@ -129,14 +121,11 @@ export default function CourseDetail({
                     {/* TODO: find out whether part below can be deleted */}
                     {/* display table component with attendance details for the course */}
                     {/* <div className="grid w-fit sm:grid-cols-1 gap-5">
-                    {/* TODO: backend: find out corresponding values for course and pass to courseDate */}
                     {/*{children}
                   </div> */}
                   </div>
                   <div className="grid gap-y-5">
                     {/* Collapsible section which contains all the groups of the current Praktikum */}
-                    {console.log("test")}
-                    {console.log(accordions)}
                     {accordions.map((accordion, index) => (
                       <Accordion
                         key={index}
@@ -194,22 +183,18 @@ export default function CourseDetail({
                   <div className="text-secondary dark:text-white">
                     {/* display courseID as determined by href url */}
                     <h1 className="mb-5 text-5xl font-bold text-center">
-                      {/* TODO: backend: find out and display course name not courseID */}
                       {courseName}
                     </h1>
                     <h1 className="mb-5 text-3xl font-bold text-center">
-                      {/* TODO: frontend: pass chosen group number to this page and display here */}
                       (ID: {blockId})
                     </h1>
                     <h1 className="mb-5 text-3xl font-bold text-center">
-                      {/* TODO: frontend: pass chosen group number to this page and display here */}
                       {selectedValue}
                     </h1>
                   </div>
                   <div>
                     {/* display table component with attendance details for the course */}
                     <div className="grid w-fit sm:grid-cols-1 gap-5">
-                      {/* TODO: backend: find out corresponding values for course and pass to courseDate */}
                       {children}
                     </div>
                   </div>
