8b5cccad5d32cb33042aec3a8eabd34bd9e0ee95
Jonas <jonas.wetzel23@gmail.com>
2023-01-11 11:37:47 +0100

    Correct database

--- tests/database.spec.js
+++ tests/database.spec.js
@@ -1,12 +1,12 @@
 const mysql = require ('mysql2/promise');
 
-(async () => {
+test("navbar contains title, logo and links to intro page", async () => {
     try {
         const connection = await mysql.createConnection({
             host: 'localhost',
             user: 'root',
             password:'@UniKoeln123',
-            database: 'scida'
+            database: 'test_db'
         });
         console.log('Connected succesfully to database');
     } catch (error){
