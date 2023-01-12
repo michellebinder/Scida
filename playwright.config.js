80b1ecfc7b41b2f28574ead00fb34a1e94278f58
Marc Peter <mpeter6@smail.uni-koeln.de>
2023-01-12 19:55:23 +0100

    Adjust test timeout and add video for tests

--- playwright.config.js
+++ playwright.config.js
@@ -40,7 +40,7 @@ const config = {
   reporter: [["html"], ["junit", { outputFile: "results.xml" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    //video: "on",
+    video: "on",
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
