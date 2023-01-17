8885b0cebb610ac150aea60445f1ce2732f50dee
feli.pn <fpreussn@smail.uni-koeln.de>
2023-01-17 12:27:11 +0100

    Add popup "sicher?" before group deletion

--- components/Accordion.js
+++ components/Accordion.js
@@ -75,7 +75,8 @@ const Accordion = ({ index, group, title, children, deleteAccordion }) => {
           </div>
           <div class="tooltip" data-tip="Gruppe löschen">
             {/* Button for deleting a group */}
-            <button onClick={handleDelete}>
+            <label
+              htmlFor="popup_delete_group">
               <svg
                 className="svg-icon fill-current text-accent hover:stroke-current ml-2"
                 viewBox="0 -1 20 27"
@@ -84,8 +85,30 @@ const Accordion = ({ index, group, title, children, deleteAccordion }) => {
               >
                 <path d="M17.114,3.923h-4.589V2.427c0-0.252-0.207-0.459-0.46-0.459H7.935c-0.252,0-0.459,0.207-0.459,0.459v1.496h-4.59c-0.252,0-0.459,0.205-0.459,0.459c0,0.252,0.207,0.459,0.459,0.459h1.51v12.732c0,0.252,0.207,0.459,0.459,0.459h10.29c0.254,0,0.459-0.207,0.459-0.459V4.841h1.511c0.252,0,0.459-0.207,0.459-0.459C17.573,4.127,17.366,3.923,17.114,3.923M8.394,2.886h3.214v0.918H8.394V2.886z M14.686,17.114H5.314V4.841h9.372V17.114z M12.525,7.306v7.344c0,0.252-0.207,0.459-0.46,0.459s-0.458-0.207-0.458-0.459V7.306c0-0.254,0.205-0.459,0.458-0.459S12.525,7.051,12.525,7.306M8.394,7.306v7.344c0,0.252-0.207,0.459-0.459,0.459s-0.459-0.207-0.459-0.459V7.306c0-0.254,0.207-0.459,0.459-0.459S8.394,7.051,8.394,7.306"></path>
               </svg>
-            </button>
+            </label>
           </div>
+          {/* Pop-up window (modal), which appears when the button "Gruppe löschen" is clicked */}
+          <input type="checkbox" id="popup_delete_group" className="modal-toggle" />
+            <div className="modal">
+              <div className="modal-box">
+                <p className="text-lg font-bold text-accent">
+                  Sind Sie sicher, dass Sie diese Gruppe löschen möchten?
+                  <br></br>Dies kann nicht rückgängig gemacht werden.
+                </p>
+                <div className="modal-action flex flex-row">
+                  <label
+                    htmlFor="popup_delete_group"
+                    onClick={handleDelete}
+                    className="btn basis-1/2"
+                  >
+                    Ja, löschen
+                  </label>
+                  <label htmlFor="popup_delete_group" className="btn basis-1/2">
+                    Nein, nicht löschen
+                  </label>
+                </div>
+              </div>
+            </div> 
         </div>
       </div>
       {/* Children elements of the accordion that are only shown when the accordion is open */}
