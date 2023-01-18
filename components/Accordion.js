01810000aaf4c2183e010b34d3d9d995ccfc5e57
Michelle Binder <mbinder1@smail.uni-koeln.de>
2023-01-18 14:33:59 +0100

    Change hover effect on delete & save btn (@felipn)

--- components/Accordion.js
+++ components/Accordion.js
@@ -64,12 +64,12 @@ const Accordion = ({ index, group, title, children, deleteAccordion }) => {
             onChange={(e) => handleChange(e.target.value)}
           ></input>{" "}
         </div>
-        <div className="ml-auto mr-5 mt-2">
+        <div className="ml-auto mr-2 mt-2">
           <div class="tooltip" data-tip="Neuen Gruppennamen speichern">
             {/* Button for saving the changes that have been made to the group name */}
-            <button onClick={handleSubmit}>
+            <button onClick={handleSubmit} className="btn btn-ghost">
               <svg
-                className="svg-icon fill-current text-primary hover:stroke-current dark:text-white"
+                className="svg-icon fill-current text-primary dark:text-white"
                 viewBox="0 2 20 20"
                 width="30"
                 height="40"
@@ -80,16 +80,18 @@ const Accordion = ({ index, group, title, children, deleteAccordion }) => {
           </div>
           <div class="tooltip" data-tip="Gruppe löschen">
             {/* Button for deleting a group */}
-            <label htmlFor="popup_delete_group">
-              <svg
-                className="svg-icon fill-current text-accent hover:stroke-current ml-2"
-                viewBox="0 -1 20 27"
-                width="30"
-                height="40"
-              >
-                <path d="M17.114,3.923h-4.589V2.427c0-0.252-0.207-0.459-0.46-0.459H7.935c-0.252,0-0.459,0.207-0.459,0.459v1.496h-4.59c-0.252,0-0.459,0.205-0.459,0.459c0,0.252,0.207,0.459,0.459,0.459h1.51v12.732c0,0.252,0.207,0.459,0.459,0.459h10.29c0.254,0,0.459-0.207,0.459-0.459V4.841h1.511c0.252,0,0.459-0.207,0.459-0.459C17.573,4.127,17.366,3.923,17.114,3.923M8.394,2.886h3.214v0.918H8.394V2.886z M14.686,17.114H5.314V4.841h9.372V17.114z M12.525,7.306v7.344c0,0.252-0.207,0.459-0.46,0.459s-0.458-0.207-0.458-0.459V7.306c0-0.254,0.205-0.459,0.458-0.459S12.525,7.051,12.525,7.306M8.394,7.306v7.344c0,0.252-0.207,0.459-0.459,0.459s-0.459-0.207-0.459-0.459V7.306c0-0.254,0.207-0.459,0.459-0.459S8.394,7.051,8.394,7.306"></path>
-              </svg>
-            </label>
+            <button className="btn btn-ghost">
+              <label htmlFor="popup_delete_group">
+                <svg
+                  className="svg-icon fill-current text-accent"
+                  viewBox="0 -1 20 27"
+                  width="30"
+                  height="40"
+                >
+                  <path d="M17.114,3.923h-4.589V2.427c0-0.252-0.207-0.459-0.46-0.459H7.935c-0.252,0-0.459,0.207-0.459,0.459v1.496h-4.59c-0.252,0-0.459,0.205-0.459,0.459c0,0.252,0.207,0.459,0.459,0.459h1.51v12.732c0,0.252,0.207,0.459,0.459,0.459h10.29c0.254,0,0.459-0.207,0.459-0.459V4.841h1.511c0.252,0,0.459-0.207,0.459-0.459C17.573,4.127,17.366,3.923,17.114,3.923M8.394,2.886h3.214v0.918H8.394V2.886z M14.686,17.114H5.314V4.841h9.372V17.114z M12.525,7.306v7.344c0,0.252-0.207,0.459-0.46,0.459s-0.458-0.207-0.458-0.459V7.306c0-0.254,0.205-0.459,0.458-0.459S12.525,7.051,12.525,7.306M8.394,7.306v7.344c0,0.252-0.207,0.459-0.459,0.459s-0.459-0.207-0.459-0.459V7.306c0-0.254,0.207-0.459,0.459-0.459S8.394,7.051,8.394,7.306"></path>
+                </svg>
+              </label>
+            </button>
           </div>
           {/* Pop-up window (modal), which appears when the button "Gruppe löschen" is clicked */}
           <input
@@ -119,7 +121,7 @@ const Accordion = ({ index, group, title, children, deleteAccordion }) => {
         </div>
       </div>
       {/* Children elements of the accordion that are only shown when the accordion is open */}
-      {isOpen && <div className="py-3 bg-gray-700">{children}</div>}
+      {isOpen && <div className="py-3 bg-base-100">{children}</div>}
     </div>
   );
 };
