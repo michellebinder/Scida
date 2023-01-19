29a24d9687840704f59f299b255a6426248e9baa
lschrays <lschrays@smail.uni-koeln.de>
2023-01-20 00:15:09 +0100

    Remove lecturers from regular updatePassword api

--- pages/api/updatePassword.js
+++ pages/api/updatePassword.js
@@ -21,11 +21,7 @@ export default async (req, res) => {
     }
 
     //Check if users role is allowed to contact api, here role A (Admin i.e. Dekanat) and B (Beschäftigte i.e Sekretariat) is allowed
-    if (
-      role === "scidaDekanat" ||
-      role === "scidaSekretariat" ||
-      role === "B"
-    ) {
+    if (role === "scidaDekanat" || role === "scidaSekretariat") {
       // Get data submitted in request's body.
       if (req.body == null) {
         res.status(400).json("FAIL CODE 1");
