43d0d6a2180f78dd662ed8c1673dc7ed135f7535
Marcin Langer <mail@mlanger.eu>
2024-02-14 13:30:35 +0100

    fix: make sure unauthenticated users are allowed to access routes necessary for authentication

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,9 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { getCsrfToken } from "next-auth/react";
 
 export const CSRF_COOKIE = 'next-auth.csrf-token'
 export const CSRF_HEADER = 'X-CSRF-TOKEN';
 
-// This function can be marked `async` if using `await` inside
 export default async function middleware(request: NextRequest, response: NextResponse) {
     if (request.method !== 'POST') {
         return NextResponse.next();
@@ -33,4 +31,14 @@ export class CsrfInvalidTokenError extends Error {
         super(`CSRF token validation failed: invalid token`);
         this.name = "CsrfInvalidTokenError";
     }
+}
+
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api/auth
+         */
+        '/((?!api/auth).*)',
+    ],
 }
